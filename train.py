a38ffb0e882c552a31087bfb71ba93b9488d896e
jlnmurthy95 <jlnmurthy95@gmail.com>
2019-07-19 18:42:01 +0900

    updated test code

--- train.py
+++ train.py
@@ -7,21 +7,23 @@ from yolov1 import yolonet
 from load_labels import VOCDataset
 from yolov1loss import yolov1loss
 
-from absl import flags
-
+from absl import flags 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('n_epochs', 1,'Number of training epochs',
 											lower_bound=0)
 flags.DEFINE_boolean('train', False,'Network set to train')
-flags.DEFINE_integer('val_frequency', 1,'Val results printing frequency',
+flags.DEFINE_boolean('resume_train', False,'--resume training operation')
+flags.DEFINE_integer('val_frequency', 1,'--val results printing frequency',
 											lower_bound=1)
 flags.DEFINE_boolean('gpu', True,'use GPU for training')
 flags.DEFINE_integer('train_batch', 16,'training batch size',
 											lower_bound=1)
 flags.DEFINE_integer('val_batch', 16,'validation batch size',
 											lower_bound=1)
-
+flags.DEFINE_string('model_path', '', 'path to model_chkpt file')
+flags.DEFINE_string('test_path', '', 'path to test file')
+flags.DEFINE_string('save_pred', '', 'path to save image with predictions')
 
 def train(argv):
 	#make a network object
@@ -34,38 +36,51 @@ def train(argv):
 	
 	loss = yolov1loss(lambda_coord=0.005, lambda_noobj=0.005, device=device)
 	optimizer = torch.optim.Adam(net.parameters())
-	traindata, valdata = VOCDataset(FLAGS.train_batch, FLAGS.val_batch)
-	train_loss, val_loss= [], []
-	for i in range(FLAGS.n_epochs):
-		trainiter = iter(traindata)
-		net.train()
-		for _ in tqdm(range(len(trainiter)), ncols=50):
-			x, y = trainiter.next()
-			x = x.to(device)
-			y = y.to(device)
-			y_hat = net(x)
-			loss_ = loss(y_hat, y)
-			train_loss.append(loss_.item())
-			optimizer.zero_grad()
-			loss_.backward()
-			optimizer.step()
- 		  
-		print('training loss after {}/{} epochs is {}'.format(i+1, FLAGS.n_epochs, np.mean(train_loss[-len(trainiter):])))	
-		if i % FLAGS.val_frequency == 0:
-			valiter = iter(valdata)
-			net.eval()
-			with torch.no_grad():
-				for _ in tqdm(range(len(valiter)), ncols=50):
-					x, y = valiter.next()
-					x = x.to(device)
-					y = y.to(device)
-					y_hat = net(x)
-					loss_ = loss(y_hat, y)
-					val_loss.append(loss_.item())
-					
-				print('validation loss after {}/{} epochs is {}'.format(i+1, FLAGS.n_epochs, np.mean(val_loss[-len(valiter):])))
-				
-if __name__ == '__main__':
+	
+	if FLAGS.resume_train:
+		checkpoint = torch.load(FLAGS.model_path)
+		net.load_state_dict(checkpoint['model_state_dict'])
+		optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+		epoch = checkpoint['epoch']
+		loss = checkpoint['loss']
+		
+	
+	if FLAGS.train :
+		traindata, valdata = VOCDataset(FLAGS.train_batch, FLAGS.val_batch)
+		train_loss, val_loss= [], []
+		for i in range(FLAGS.n_epochs):
+			trainiter = iter(traindata)
+			net.train()
+			for _ in tqdm(range(len(trainiter)), ncols=50):
+				x, y = trainiter.next()
+				x, y = x.to(device), y.to(device)
+				y_hat = net(x)
+				loss_ = loss(y_hat, y)
+				train_loss.append(loss_.item())
+				optimizer.zero_grad()
+				loss_.backward()
+				optimizer.step()
+ 			  
+			print('training loss after {}/{} epochs is {}'.format(i+1, FLAGS.n_epochs, np.mean(train_loss[-len(trainiter):])))	
+			
+			if i % FLAGS.val_frequency == 0:
+				valiter = iter(valdata)
+				net.eval()
+				with torch.no_grad():
+					for _ in tqdm(range(len(valiter)), ncols=50):
+						x, y = valiter.next()
+						x, y = x.to(device), y.to(device)
+						y_hat = net(x)
+						loss_ = loss(y_hat, y)
+						val_loss.append(loss_.item())
+						
+					print('validation loss after {}/{} epochs is {}'.format(i+1, FLAGS.n_epochs, np.mean(val_loss[-len(valiter):])))
+	
+	else:
+		
+	
+							
+if__name__ == '__main__':
 	from absl import app
 	app.run(train)
 			 
--- utils.py
+++ utils.py
@@ -3,7 +3,16 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches 
 from PIL import Image
 import numpy as np
+from absl import flags, app
+
 from class_names import class_names
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_float('iou_threshold', 0.6, 
+									'--minimum iou for box rejection')
+flags.DEFINE_float('obj_threshold', 0.25, 
+									'--minimum score for box consideration')
 
 np.random.seed(100)
 color_dict = np.random.rand(20,3)
@@ -13,7 +22,6 @@ def visualize_bbox(image, label_tensor, save_path=None):
 	object_mask = object_mask.unsqueeze(dim=-1).expand(label_tensor.shape)
 	#arrange the values into a tensor of box coordinates 
 	label_tensor = label_tensor[object_mask].view((-1, 6))
-	print(label_tensor) 
 	scale_x, scale_y = image.size(1), image.size(2)
 	centers = label_tensor[:,0:2]
 	sizes = label_tensor[:,2:4]
@@ -21,7 +29,6 @@ def visualize_bbox(image, label_tensor, save_path=None):
 	label_tensor[:,0:2] = label_tensor[:,0:2] * torch.tensor([scale_x, scale_y], dtype=torch.float32)
 	label_tensor[:,2:4] = label_tensor[:,2:4] * torch.tensor([scale_x, scale_y], dtype=torch.float32)
 	label_tensor = label_tensor.to(torch.int16)
-	print (label_tensor)
 	fig, ax = plt.subplots(1)
 	ax.imshow(image.permute(1,2,0))
 
@@ -33,10 +40,73 @@ def visualize_bbox(image, label_tensor, save_path=None):
 	if save_path is not None:
 		plt.savefig(save_path)
 	
+def IoU(bb1, bb2):
+	'''
+	bb1 : input bounding box 1 the coordinates corresponds to 
+	      center_x center_y width height
+	bb2 : bounding box 2 coordinates in the same order as bb1
+	'''
+	minx = min(bb1[0] - bb1[2] / 2, bb2[0] - bb2[2] / 2)
+	maxx = max(bb1[0] + bb1[2] / 2, bb2[0] + bb2[2] / 2)
+	miny = min(bb1[1] - bb1[3] / 2, bb2[1] - bb2[3] / 2)
+	maxy = max(bb1[1] + bb1[3] / 2, bb2[1] + bb2[3] / 2)
+	diffx = (maxx - minx) - (bb1[2] + bb2[2]) 
+	diffy = (maxy - miny) - (bb1[3] + bb2[3])
+	if diffx < 0  and diffy < 0 :
+		intersection = diffx * diffy
+		return intersection / (bb1[2] * bb1[3] + bb2[2] * bb2[3] - intersection) 
+	return 0
 
-
+def non_max_suppression( predicted_tensor):
+	#grid coordinates to add to predicted offset
+	predicted_tensor = predicted_tensor.cpu()
+	x = torch.tensor(torch.arange(7))
+	y = torch.tensor(torch.arange(7))
+	grid_x, grid_y = torch.meshgrid(x, y)
+	box1, box2, classes = predicted_tensor.split([5, 5, 20], 1)
+	classes = torch.argmax(classes, 1, keepdim=True).type(torch.float)
+	grid = torch.cat([grid_x.unsqueeze(0), grid_y.unsqueeze(0)], 0).type(torch.float)
+	box1_coords, box1_dims, box1_confs = box1.split([2,2,1], 1)
+	box2_coords, box2_dims, box2_confs = box2.split([2,2,1], 1)
+	box1_coords = (box1_coords + grid)/7
+	box2_coords = (box2_coords + grid)/7
+	box1 = torch.cat([box1_coords, box1_dims, classes, box1_confs], dim=1).permute(0, 2, 3, 1)
+	box2 = torch.cat([box2_coords, box2_dims, classes, box2_confs], dim=1).permute(0, 2, 3, 1)
+	box1 = box1.view(box1.shape[0], -1, box1.shape[3])
+	box2 = box2.view(box2.shape[0], -1, box2.shape[3]) 
+	boxes = torch.cat([box1, box2], 1).numpy()
+	obj_mask = boxes[:, :, 5] >= FLAGS.obj_threshold
+	selected_boxes = []
+	
+	for box, mask in zip(boxes, obj_mask):
+		mask = mask.reshape(-1)
+		this_boxes = []
+		temp_boxes = []
+		for i in range(mask.shape[0]):	
+			if mask[i]:
+				temp_boxes.append(box[i])
 		
+		box = sorted(temp_boxes, key=lambda a:a[-2])
+		box_list = [[] for _ in range(20)]
+		
+		for i in range(len(box)):
+			box_list[int(box[i][-2])].append(box[i]) 
+		for sorted_ in box_list:
+			while len(sorted_) > 0:
+				this_box = sorted_[0]
+				del sorted_[0]
+				sorted_new = []
+				for i in range(len(sorted_)):
+					if not IoU(this_box, sorted_[i]) > FLAGS.iou_threshold:
+						sorted_new.append(sorted_[i])
+
+				sorted_ = sorted_new
+				this_boxes.append(this_box)
+		selected_boxes.append(torch.tensor(this_boxes))
+	return selected_boxes
 	
-	
-	
-	
+if __name__ == '__main__':
+	random_prediction = torch.rand([1, 30, 7, 7], device='cuda')	
+	def run_(argv):
+		visualize_bbox('test.png', non_max_suppression(random_prediction)[0])
+	app.run(run_)
